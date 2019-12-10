478917c355f68d8fb5c0931333d0e8ee18fb337e
scoutskylar <45772202+scoutskylar@users.noreply.github.com>
2019-12-09 22:42:03 -0500

    Redesign

--- load.js
+++ load.js
@@ -1,4 +1,7 @@
 {
+  const newThemeAvailable = true;
+  const newThemeAltText = 'FHSched New Theme';
+  const newThemeAltTextHint = 'Icy background with snowflakes falling';
   const themes = [
     'Classic',
     'Dark',
@@ -16,6 +19,7 @@
 
   let main = document.getElementsByTagName('main')[0];
   let div, img, p;
+  let themeIndex = 1;
   for (themeName of themes) {
     div = document.createElement('div');
     div.setAttribute('data-theme', themeName);
@@ -32,10 +36,29 @@
     div.appendChild(p);
 
     main.appendChild(div);
+
+    themeIndex++;
+  }
+
+  if (newThemeAvailable) {
+    div = document.createElement('div');
+    
+    // Thumbnail
+    img = document.createElement('img');
+    img.setAttribute('src', 'new theme.jpg?index=' + themeIndex);
+    img.setAttribute('alt', ((newThemeAltText ? '“' + newThemeAltText + '”' + (newThemeAltTextHint ? ': ' : '') : '') + newThemeAltTextHint));
+    div.appendChild(img);
+    
+    // Title
+    p = document.createElement('p');
+    p.innerText = 'New Theme';
+    div.appendChild(p);
+
+    main.appendChild(div);
   }
 }
 
 // <div>
-//     <img src="cornucopia.png" alt="Cornucopia">
+//     <img src="thumbnails/cornucopia.png" alt="Cornucopia">
 //     <p>Cornucopia</p>
 // </div>
